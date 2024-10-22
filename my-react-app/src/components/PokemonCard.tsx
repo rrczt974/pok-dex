6ccf3f417ac5248b749718b075d556449c9d0dfa
rrczt974 <raphaelriviere87@gmail.com>
2024-10-22 13:46:20 +0200

    [test] solve quest

--- my-react-app/src/components/PokemonCard.tsx
+++ my-react-app/src/components/PokemonCard.tsx
@@ -1,7 +1,27 @@
 function PokemonCard() {
-   return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="image qui représente un Pokémon Bulbasaur"></img>
-        <figcaption>Bulbasaur</figcaption>
-    </figure>
+    const pokemon = pokemonList[1];
+    
+    return <figure>
+        <img src={pokemon.imgSrc !== "" ? pokemon.imgSrc : "pas de source"}
+             alt={pokemon.imgSrc === true ? pokemon.name : "???" } />
+          
+          <figcaption>{pokemon.name}</figcaption>
+          </figure>
 }
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+
 export default PokemonCard;
+
+// Modifie ton code pour afficher l'image du Pokémon seulement si pokemon.imgSrc est défini. 
+// Si l'image est disponible, utilise une balise <img> pour l'afficher. Sinon, affiche un paragraphe <p> avec "???" en texte. Utilise l'opérateur ternaire ?: pour cela.
