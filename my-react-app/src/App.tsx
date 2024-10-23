fd2a5e30fee4dbbcffcc59c642ce1ab2c1d730a5
rrczt974 <raphaelriviere87@gmail.com>
2024-10-23 21:31:06 +0200

    [test] solve quest5 props2

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -22,7 +22,7 @@ const pokemonList = [
 function App () {
 return (
   <>
-    <PokemonCard pokemon={pokemonList[1]} />
+    <PokemonCard pokemon={pokemonList[0]} />
   </>
 )
 }
--- my-react-app/src/components/PokemonCard.tsx
+++ my-react-app/src/components/PokemonCard.tsx
@@ -1,15 +1,17 @@
-// Mes props pokemon
+// interface props
+interface PokemonProps {
+	imgSrc? : string;
+	name : string;
+}
 
 // ma fonction pokemonCards
-function PokemonCard(props) {
-	const {pokemon} = props;
+function PokemonCard({pokemon} : PokemonProps) {
 	
-
 	return (
 		<figure>
 			<img
-				src={pokemon.imgSrc !== "" ? pokemon.imgSrc : ""} 
-				alt={pokemon.imgSrc !== "" ? "???" : pokemon.name}
+				src={pokemon.imgSrc ? pokemon.imgSrc : ""} 
+				alt={pokemon.imgSrc ? pokemon.name : "???"}
 				/>
 			<figcaption> {pokemon.name} </figcaption>
 		</figure>
