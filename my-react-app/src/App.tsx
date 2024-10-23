dc111f049e35cb4fc5f01b65a1e64f120d4035ee
rrczt974 <raphaelriviere87@gmail.com>
2024-10-23 20:57:27 +0200

    [test] solve quest 4 props 1

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -5,10 +5,26 @@ import "./App.css";
 import MyTitle from "./components/MyTitle";
 import PokemonCard from "./components/PokemonCard";
 
-<div>
-  <main>
-    <PokemonCard />
-  </main>
-</div>
+// ma pokemon list 
+const pokemonList = [
+	{
+	  name: "bulbasaur",
+	  imgSrc:
+		"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+	{
+	  name: "mew",
+	},
+  ];
+  
 
-export default PokemonCard;
+// Ma fonction App
+function App () {
+return (
+  <>
+    <PokemonCard pokemon={pokemonList[1]} />
+  </>
+)
+}
+
+export default App;
--- my-react-app/src/components/PokemonCard.tsx
+++ my-react-app/src/components/PokemonCard.tsx
@@ -1,27 +1,22 @@
-function PokemonCard() {
-    const pokemon = pokemonList[1];
-    
-    return <figure>
-        <img src={pokemon.imgSrc !== "" ? pokemon.imgSrc : "pas de source"}
-             alt={pokemon.imgSrc === true ? pokemon.name : "???" } />
-          
-          <figcaption>{pokemon.name}</figcaption>
-          </figure>
+// Mes props pokemon
+
+// ma fonction pokemonCards
+function PokemonCard(props) {
+	const {pokemon} = props;
+	
+
+	return (
+		<figure>
+			<img
+				src={pokemon.imgSrc !== "" ? pokemon.imgSrc : ""} 
+				alt={pokemon.imgSrc !== "" ? "???" : pokemon.name}
+				/>
+			<figcaption> {pokemon.name} </figcaption>
+		</figure>
+	);
 }
 
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-
-
 export default PokemonCard;
-
-// Modifie ton code pour afficher l'image du Pokémon seulement si pokemon.imgSrc est défini. 
-// Si l'image est disponible, utilise une balise <img> pour l'afficher. Sinon, affiche un paragraphe <p> avec "???" en texte. Utilise l'opérateur ternaire ?: pour cela.
+// 
+				
+//={pokemon[1].imgSrc} // = true ? pokemon[0].name : "???"}
